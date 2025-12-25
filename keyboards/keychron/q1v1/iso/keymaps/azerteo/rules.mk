0b182f3abfbbdb4fca8916721a8ca3806a71a135
pierre <pierre@device-70.home>
2025-12-25 17:03:53 +0100

    Add MAC layout (colors not fully workings).

--- keyboards/keychron/q1v1/iso/keymaps/azerteo/rules.mk
+++ keyboards/keychron/q1v1/iso/keymaps/azerteo/rules.mk
@@ -1,6 +1,8 @@
-#UNICODE_COMMON = yes
 UNICODEMAP_ENABLE = yes
 $(info   Unicode common and unicode map enabled.)
 
-#CONSOLE_ENABLE = yes
 KEY_OVERRIDE_ENABLE = yes
+
+DYNAMIC_MACRO_ENABLE = yes
+#For managing the knob.
+#ENCODER_ENABLE = yes
