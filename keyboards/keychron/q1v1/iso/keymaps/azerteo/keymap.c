aa19e15d26d0dbf29af6bcff35d0bcb221694fa5
Pierre Vittet <pvittet@murena.io>
2026-01-10 15:39:15 +0100

    Manage the star symbol + the % for eo keyboard

--- keyboards/keychron/q1v1/iso/keymaps/azerteo/keymap.c
+++ keyboards/keychron/q1v1/iso/keymaps/azerteo/keymap.c
@@ -61,6 +61,7 @@ enum unicode_names {
     EO_HAU,
     EO_CA,
     EO_CAU,
+    EO_STR,
 };
 
 const uint32_t unicode_map[] PROGMEM = {
@@ -76,6 +77,7 @@ const uint32_t unicode_map[] PROGMEM = {
     [EO_CAU] = 0x0108,  // Ĉ
     [EO_HA]  = 0x0125,  // ĥ
     [EO_HAU] = 0x0124,  // Ĥ
+    [EO_STR] = 0x272D,  // Star
 };
 
 //%%%%%%%%%%%%%%%%%%%%%%%%%%%% END ESPERANTO UNICODE %%%%%%%%%%%%%%%%%%%%%%%%%%%% 
@@ -118,19 +120,19 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
 
     [MAC_FN] = LAYOUT_iso_83(
             //Base MAC function layer
-        _______,            KC_F1,    KC_F2,    KC_F3,    KC_F4,    KC_F5,    KC_F6,    KC_F7,    KC_F8,    KC_F9,    KC_F10,   KC_F11,         KC_F12,   _______,  _______,
+        _______,  KC_F1,    KC_F2,    KC_F3,    KC_F4,    KC_F5,    KC_F6,    KC_F7,    KC_F8,    KC_F9,    KC_F10,   KC_F11,   KC_F12,         _______,            _______,
         _______,  _______,  _______,  _______,  _______,  _______,  _______,  _______,  _______,  _______,  _______,  _______,  _______,        _______,            _______,
         RM_TOGG,  RM_NEXT,  RM_VALU,  RM_HUEU,  RM_SATU,  RM_SPDU,  _______,  _______,  _______,  _______,  _______,  _______,  _______,                            _______,
         _______,  RM_PREV,  RM_VALD,  RM_HUED,  RM_SATD,  RM_SPDD,  _______,  _______,  _______,  _______,  _______,  _______,  _______,        _______,            _______,
         _______,  _______,  _______,  _______,  _______,  _______,  _______,  NK_TOGG,  _______,  _______,  _______,  _______,                  _______,  _______,
         _______,  _______,  _______,                                _______,                                _______,  _______,  DF(MACEO_BASE), _______,  _______,  _______),
     [MACEO_BASE] = LAYOUT_iso_83(
-        KC_ESC,                     KC_BRID,  KC_BRIU,          KC_NO,    KC_NO,    RM_VALD,            RM_VALU,  KC_MPRV,  KC_MPLY,  KC_MNXT,  KC_MUTE,  KC_VOLD,  KC_VOLU,  KC_DEL,   KC_INS,
-        KC_GRV,   KC_1,             KC_2,     KC_3,             KC_4,     KC_5,     KC_6,               KC_7,     KC_8,     KC_9,     KC_0,     KC_MINS,  KC_EQL,   KC_BSPC,            KC_PGUP,
-        KC_TAB,   KC_Q,             KC_W,     KC_E,             KC_R,     KC_T,     UP(EO_UA,EO_UAU),   KC_U,     KC_I,     KC_O,     KC_P,     KC_LBRC,  KC_RBRC,                      KC_PGDN,
-        KC_CAPS,  UP(EO_SA,EO_SAU), KC_S,     KC_D,             KC_F,     KC_G,     UP(EO_GA, EO_GAU),  KC_J,     KC_K,     KC_L,     KC_SCLN,  KC_QUOT,  KC_NUHS,  KC_ENT,             KC_HOME,
-        KC_LSFT,  UP(EO_HA, EO_HAU),KC_H,     UP(EO_CA, EO_CAU),KC_C,     KC_V,     KC_B,               KC_N,     KC_M,     KC_COMM,  KC_DOT,   KC_SLSH,            KC_RSFT,  KC_UP,
-        KC_LCTL,  KC_LOPT,          KC_LCMD,                                KC_SPC,                                           KC_RCMD, MO(MACEO_FN),KC_RCTL,  KC_LEFT,  KC_DOWN,  KC_RGHT),
+        KC_ESC,         KC_BRIU,          KC_BRID,  KC_TASK,          KC_FLXP,  KC_MPRV,  KC_MPLY,            KC_MNXT,  KC_PSCR,  KC_MUTE,  KC_VOLD,  KC_VOLU,  KC_SLEP,  KC_DEL,             KC_INS,
+        LSFT(KC_QUOT),  KC_1,             KC_2,     KC_3,             KC_4,     KC_5,     KC_6,               KC_7,     KC_8,     KC_9,     KC_0,     KC_MINS,  KC_EQL,   KC_BSPC,            KC_PGUP,
+        KC_TAB,         KC_Q,             KC_W,     KC_E,             KC_R,     KC_T,     UP(EO_UA,EO_UAU),   KC_U,     KC_I,     KC_O,     KC_P,     KC_LBRC,  UP(EO_STR,KC_RBRC),           KC_PGDN,
+        KC_CAPS,        UP(EO_SA,EO_SAU), KC_S,     KC_D,             KC_F,     KC_G,     UP(EO_GA, EO_GAU),  KC_J,     KC_K,     KC_L,     KC_SCLN,  KC_QUOT,  KC_NUHS,  KC_ENT,             KC_HOME,
+        KC_LSFT,        UP(EO_HA, EO_HAU),KC_H,     UP(EO_CA, EO_CAU),KC_C,     KC_V,     KC_B,               KC_N,     KC_M,     KC_COMM,  KC_DOT,   KC_SLSH,            KC_RSFT,  KC_UP,
+        KC_LCTL,        KC_LOPT,          KC_LCMD,                                KC_SPC,                                           KC_RCMD, MO(MACEO_FN),KC_RCTL,  KC_LEFT,  KC_DOWN,  KC_RGHT),
     [MACEO_FN] = LAYOUT_iso_83(
         _______,            KC_F1,    KC_F2,    KC_F3,    KC_F4,    KC_F5,    KC_F6,    KC_F7,    KC_F8,    KC_F9,    KC_F10,   KC_F11,   KC_F12,   _______,  _______,
         _______,  _______,  _______,  _______,  _______,  _______,  _______,  _______,  _______,  _______,  _______,  _______,  _______,  _______,            _______,
@@ -148,7 +150,10 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
         KC_LCTL,  KC_LWIN,  KC_LALT,                                KC_SPC,                                 KC_RALT, MO(WINAZ_FN),KC_RCTL,  KC_LEFT,  KC_DOWN,  KC_RGHT),
     [WINAZ_FN] = LAYOUT_iso_83(
             //Base Windows function layer with right control rebased to EO layer
-        _______,            KC_BRID,  KC_BRIU,  KC_TASK,  KC_FLXP,  RM_VALD,  RM_VALU,  KC_MPRV,  KC_MPLY,  KC_MNXT,  KC_MUTE,  KC_VOLD,  KC_VOLU,  _______,  _______,
+            //In a next version  KC_TASK and KC_FLXP should be replaced by others keys because they are not very
+            //important and they are OS dependant (non standard).
+     // _______,            KC_BRIU,  KC_BRID,  KC_TASK,  KC_FLXP,  KC_MPRV,  KC_MPLY,  KC_MNXT,  KC_PSCR,  KC_MUTE,  KC_VOLD,  KC_VOLU,  KC_SLEP,  _______,  _______,
+        _______,            KC_BRIU,  KC_BRID,  DM_REC1,  DM_PLY1,  KC_MPRV,  KC_MPLY,  KC_MNXT,  KC_PSCR,  KC_MUTE,  KC_VOLD,  KC_VOLU,  KC_SLEP,  _______,  _______,
         _______,  _______,  _______,  _______,  _______,  _______,  _______,  _______,  _______,  _______,  _______,  _______,  _______,  _______,            _______,
         RM_TOGG,  RM_NEXT,  RM_VALU,  RM_HUEU,  RM_SATU,  RM_SPDU,  _______,  _______,  _______,  _______,  _______,  _______,  _______,                      _______,
         _______,  RM_PREV,  RM_VALD,  RM_HUED,  RM_SATD,  RM_SPDD,  _______,  _______,  _______,  _______,  _______,  _______,  _______,  _______,            _______,
@@ -156,15 +161,15 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
         DB_TOGG,  _______,  _______,                                _______,                                TG(FN_KEY), _______,  DF(AZEO_BASE),  _______,  _______,  _______),
     [AZEO_BASE] = LAYOUT_iso_83(
             //New azerty EO layer. Mostly azerty layer with some rebase for EO keys
-        KC_ESC,                     KC_P1, KC_P2,              KC_P3,    KC_P4,    KC_P5,              KC_P6,    KC_P7,    KC_P8,    KC_P9,    KC_P0 ,             KC_PPLS,  KC_PAST,  KC_DEL,   KC_INS,
-        KC_GRV,   KC_1,             KC_2,  KC_3,               KC_4,     KC_5,     KC_6,               KC_7,     KC_8,     KC_9,     KC_0,     KC_MINS,            KC_EQL,   KC_BSPC,            KC_PGUP,
-        KC_TAB,   KC_Q,             KC_W,  KC_E,               KC_R,     KC_T,     UP(EO_UA,EO_UAU),   KC_U,     KC_I,     KC_O,     KC_P,     KC_NUBS,            KC_RBRC,                      KC_PGDN,
-        KC_CAPS,  UP(EO_SA,EO_SAU), KC_S,  KC_D,               KC_F,     KC_G,     UP(EO_GA, EO_GAU),  KC_J,     KC_K,     KC_L,     KC_SCLN,  UP(EO_JA, EO_JAU),  KC_NUHS,  KC_ENT,             KC_HOME,
-        KC_LSFT,  UP(EO_HA, EO_HAU),KC_H,  UP(EO_CA, EO_CAU),  KC_C,     KC_V,     KC_B,               KC_N,     KC_M,     KC_COMM,  KC_DOT,   KC_SLSH,            KC_RSFT,            KC_UP,
-        KC_LCTL,  KC_LWIN,          KC_LALT,                                KC_SPC,                                                  KC_RALT,  MO(AZEO_FN),KC_RCTL,          KC_LEFT,  KC_DOWN,  KC_RGHT),
+        KC_ESC,         KC_P1,            KC_P2, KC_P3,              KC_P4,    KC_P5,    KC_P6,              KC_P7,    KC_P8,    KC_P9,    KC_P0 ,   KC_PPLS,            KC_PAST,  KC_DEL,             KC_INS,
+        LSFT(KC_QUOT),  KC_1,             KC_2,  KC_3,               KC_4,     KC_5,     KC_6,               KC_7,     KC_8,     KC_9,     KC_0,     KC_MINS,            KC_EQL,   KC_BSPC,            KC_PGUP,
+        KC_TAB,         KC_Q,             KC_W,  KC_E,               KC_R,     KC_T,     UP(EO_UA,EO_UAU),   KC_U,     KC_I,     KC_O,     KC_P,     KC_NUBS,            UP(EO_STR,KC_RBRC),                      KC_PGDN,
+        KC_CAPS,        UP(EO_SA,EO_SAU), KC_S,  KC_D,               KC_F,     KC_G,     UP(EO_GA, EO_GAU),  KC_J,     KC_K,     KC_L,     KC_SCLN,  UP(EO_JA, EO_JAU),  KC_NUHS,  KC_ENT,             KC_HOME,
+        KC_LSFT,        UP(EO_HA, EO_HAU),KC_H,  UP(EO_CA, EO_CAU),  KC_C,     KC_V,     KC_B,               KC_N,     KC_M,     KC_COMM,  KC_DOT,   KC_SLSH,            KC_RSFT,            KC_UP,
+        KC_LCTL,        KC_LWIN,          KC_LALT,                                KC_SPC,                                                  KC_RALT,  MO(AZEO_FN),KC_RCTL,          KC_LEFT,  KC_DOWN,  KC_RGHT),
 
     [AZEO_FN] = LAYOUT_iso_83(
-        _______,            KC_BRID,  KC_BRIU,  KC_TASK,  KC_FLXP,  RM_VALD,  RM_VALU,  KC_MPRV,  KC_MPLY,  KC_MNXT,  KC_MUTE,  KC_VOLD,  KC_VOLU,  _______,  _______,
+        _______,            KC_BRIU,  KC_BRID,  KC_TASK,  KC_FLXP,  KC_MPRV,  KC_MPLY,  KC_MNXT,  KC_PSCR,  KC_MUTE,  KC_VOLD,  KC_VOLU,  KC_SLEP,  _______,  _______,
         _______,  _______,  _______,  _______,  _______,  _______,  _______,  _______,  _______,  _______,  _______,  _______,  _______,  _______,            _______,
         RM_TOGG,  RM_NEXT,  RM_VALU,  RM_HUEU,  RM_SATU,  RM_SPDU,  _______,  _______,  _______,  _______,  _______,  _______,  _______,                      _______,
         _______,  RM_PREV,  RM_VALD,  RM_HUED,  RM_SATD,  RM_SPDD,  _______,  _______,  _______,  _______,  _______,  _______,  _______,  _______,            _______,
